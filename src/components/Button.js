3c255b52b607018cc55ede70a001ece25ae437da
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-27 23:01:31 +0000

    prop types added to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 
 const Button = ({ text, color }) => {
     return <button className='btn' style={{ backgroundColor: color }}>{text}</button>
+}
+
+// Adds default props incase text and color aren't passed in
+Button.defaultProps = {
+    text: 'Button Text',
+    color: 'Black',
+}
+
+// Causes both props to expect a string
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
 }
 
 export default Button
