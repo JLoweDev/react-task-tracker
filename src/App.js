ecad779ae9f803881ff5cc2be4a54c50861e041d
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-28 13:02:18 +0000

    state moved to top level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Walk Dog',
+        day: 'Feb 7th at 3:45pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Walk Panther',
+        day: 'Feb 7th at 5:45pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Walk Gorilla',
+        day: 'Feb 8th at 12:45am',
+        reminder: false,
+    },
+  ])
+
   return (
     <div className="container">
       <Header title='React Task Tracker' />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,28 +1,4 @@
-import { useState } from 'react'
-
-const Tasks = () => {
-    const [tasks, setTasks] = useState([
-        {
-            id: 1,
-            text: 'Walk Dog',
-            day: 'Feb 7th at 3:45pm',
-            reminder: true,
-        },
-        {
-            id: 2,
-            text: 'Walk Panther',
-            day: 'Feb 7th at 5:45pm',
-            reminder: true,
-        },
-        {
-            id: 3,
-            text: 'Walk Gorilla',
-            day: 'Feb 8th at 12:45am',
-            reminder: false,
-        },
-    ])
-
-    
+const Tasks = ({ tasks }) => {
     return (
         <>
             {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
