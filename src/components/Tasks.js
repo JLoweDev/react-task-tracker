4d330a2c3fb43886298b132a2eee43917f325a1f
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-28 12:50:06 +0000

    Tasks.js created and filler tasks added

---
+++ src/components/Tasks.js
@@ -0,0 +1,30 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Walk Dog',
+        day: 'Feb 7th at 3:45pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Walk Panther',
+        day: 'Feb 7th at 5:45pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Walk Gorilla',
+        day: 'Feb 8th at 12:45am',
+        reminder: false,
+    },
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
