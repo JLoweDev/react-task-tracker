5bc02dc0330b20730bca71b0e07a41087d93454a
James <67014970+JLoweDev@users.noreply.github.com>
2021-01-28 12:58:56 +0000

    useState imported for Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,32 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Walk Dog',
-        day: 'Feb 7th at 3:45pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Walk Panther',
-        day: 'Feb 7th at 5:45pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Walk Gorilla',
-        day: 'Feb 8th at 12:45am',
-        reminder: false,
-    },
-]
+import { useState } from 'react'
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Walk Dog',
+            day: 'Feb 7th at 3:45pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Walk Panther',
+            day: 'Feb 7th at 5:45pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Walk Gorilla',
+            day: 'Feb 8th at 12:45am',
+            reminder: false,
+        },
+    ])
+
+    
     return (
-        <div>
-            
-        </div>
+        <>
+            {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
+        </>
     )
 }
 
